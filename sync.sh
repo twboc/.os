5cfdd625b0615229dd4c4ab8002fca3306cd06e4
twboc <hubert.sobkow@gmail.com>
2024-08-28 22:01:44 +0200

    picom sync

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 
 
-mkdir -p ./config/terminator && cp -p ~/.config/terminator/config ./config/terminator/config
+mkdir -p ./config/terminator && cp -p ~/.config/terminator/config ~/.os/config/terminator/config
+mkdir -p ./config/picom && cp -p ~/.config/picom/picom.conf ~/.os/config/picom/picom.conf
