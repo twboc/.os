6a2e3e1a6c3f6e905eba5ef708c80798820493c9
twboc <hubert.sobkow@gmail.com>
2024-09-01 03:07:48 +0200

    sweet sudo alias asdf

--- src/alias.sh
+++ src/alias.sh
@@ -1,4 +1,8 @@
 
+#alias sudo='sudo '
+
+alias asdf='sudo $(fc -ln -1)'
+
 alias pkr="pokemon-colorscripts --random"
 alias gnome="env XDG_CURRENT_DESKTOP=GNOME gnome-control-center > /dev/null 2>&1 &"
 
